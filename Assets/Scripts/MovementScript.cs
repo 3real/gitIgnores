14941ab57b358bf9a73da6de8581daaa41febb14
3real <mikegrahameverett@gmail.com>
2017-10-31 23:08:40 +0000

    init commit

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour {
+
+Animator anim;
+		
+//Constants
+
+
+
+
+void Start () {
+		anim = GetComponent<Animator> ();
+	}
+
+
+// Update is called once per frame
+void Update () {
+		if (Input.GetKeyDown(KeyCode.W))
+			{
+			anim.Play( "forward" );
+			}
+
+		if (Input.GetKeyDown(KeyCode.A))
+			{
+			anim.Play( "left" );
+			}
+		if (Input.GetKeyDown(KeyCode.D))
+		{
+			anim.Play( "right" );
+		}
+		if (Input.GetKeyDown(KeyCode.S))
+		{
+			anim.Play( "back" );
+		}
+//		else
+//		{
+//			anim.Play( "idle" );
+//		}
+}
+	}
